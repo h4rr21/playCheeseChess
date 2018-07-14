131fdead67705e0acb0687d4490373c1da5a0ba0
Juan Carlos <h4rry@Juans-MacBook-Pro.local>
2018-07-14 01:39:11 -0500

    printing chess board

---
+++ frontend/board/src/index.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Chess = require('react-chess')
+
+ReactDOM.render(<Chess />, document.getElementById('root'))
