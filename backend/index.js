90fac603a8928675b8670555fad01ab66cce0e51
Juan Carlos <h4rry@Juans-MacBook-Pro.local>
2018-07-13 19:24:47 -0500

    socket io

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,62 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-
+const chess = require('chess');
+ 
 const app = express();
 
 var port = 5000;
 var server = http.createServer(app);
 var io = socketIO(server);
 
-var jugadas = []
+var jugadas = [];
+const gameClient = chess.create();
+
+// Test
+// const gameClient = chess.create();
+// status = gameClient.getStatus();
+// console.log("game created -> ",status);
+// console.log("valid movements ->",status.notatedMoves)
+
+// var moveToValidate = 'a5'
+// if (status.notatedMoves[moveToValidate]){
+//     console.log("valid move found")
+// }else{
+//     console.log("invalid move found")
+// }
+
 
 io.on('connection',(socket)=>{
-    console.log('usuario conectado');
+    status = gameClient.getStatus();
+    console.log("game created -> ",status);
+    console.log("valid movements ->",status.notatedMoves)
+
+    socket.on('move',(oneMove)=>{
+        status = gameClient.getStatus();
+        if (status.notatedMoves[oneMove]){
+            console.log("valid move found",oneMove)
+            jugadas.push(oneMove);
+            move = gameClient.move(oneMove);
+            socket.emit('validMove',oneMove)
+        }else{
+            console.log("invalid move found: ",oneMove)
+            socket.emit('invalidMove',oneMove)
+        }
+        
+    });
+
+    // capture check and checkmate events
+    socket.on('check', (attack) => {
+    // get more details about the attack on the King
+    console.log(attack);
+    });
+
+    socket.on('resetGame',()=>{
+        gameClient = chess.create();
+        jugadas
+    });
 });
 
 server.listen(port, ()=>{
     console.log('Server listenning on port: '+port)
-})
+}) 
